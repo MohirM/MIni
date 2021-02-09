45147b29d7b530518fb66473c7abe37713f1173c
Mom <mohir591@gmail.com>
2021-02-09 11:53:30 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+/*
+function copy() 
+{
+        let tableau_2 = ['ghfg'];
+		tableau_2.forEach(element => 
+			{
+			  tableau_2 = element + tableau_2;
+			      console.log(tableau_2);
+            });
+}
+ copy();
+
+
+/*
+
+  i = 0; 
+               
+  while (i <= 9)   
+ {
+     j = i + 1;
+     while (j <= 9)
+     {
+         k = j + 1;
+         while (k <= 9)  
+         {
+             console.log(i,j,k)
+             if (!(i == 7 && j == 8 && k == 9))  
+                 console.log(', ');
+             k++;
+         }
+         j++;       
+     }
+     i++;
+ }
+
+*/
+    
